01f6a06b0eddf3d69dd2f10cfd73ad28aa7bc0f4
vgj24 <vrushlijoshi24@gmail.com>
2022-01-30 19:48:35 +0530

    Generics at method level

---
+++ GenericsDemo/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    internal class Class1
+    {
+        //h iam vrushali joshi muley
+
+        //lkdklbndfb;flk;llftthkdlglkh;ddtjdgjgdddgjgjer;gggssgrgf
+
+        public static void Display<T>(T[] test)
+        {
+
+
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.Write(test[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GenericsDemo;
+
+Console.WriteLine("Generic method");
+int[] intArray = { 2, 4, 5, 6, 8 };
+char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+double[] doubleArray = { 1.1, 3.3, 5.5, 8.7 };
+
+Class1.Display<int>(intArray);
+Class1.Display<char>(charArray);
+Class1.Display<double>(doubleArray);
+Console.ReadKey();
+
